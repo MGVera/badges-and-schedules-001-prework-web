ffd951bb173e308fc792c040e0d8ac4e35282d0d
MGVera <madelinegvera@gmail.com>
2015-12-25 02:00:59 +0000

    nitrous-backup at Fri Dec 25 02:00:59 UTC 2015

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+#
+
+
+
+
+def badge_maker(name = Arel)
+  "Hello, my name is #{name}."
+end
+
+def badge_creator(attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+  badge_creator <<badges
+                  {["Hello, my name is .",
+                  "Hello, my name is Ada.",
+                  "Hello, my name is Charles.",
+                  "Hello, my name is Alan.",
+                  "Hello, my name is Grace.",
+                  "Hello, my name is Linus.",
+                  "Hello, my name is Matz."]}
+                  badges
+end
+
+def assign_rooms
+end
+
+def printer
+end
